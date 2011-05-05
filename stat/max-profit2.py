dcfd0c75bd2bb962133a5301c94fd2d5aec8c946
cocosx <cocosx@gmail.com>
2011-05-05 19:25:57 +0200

    3.4

--- stat/max-profit2.py
+++ stat/max-profit2.py
@@ -38,23 +38,27 @@ with open(sys.argv[1], newline='') as f:
       #print(k,s/len(d[k]))
   fee=list(avgs.keys())
   fee.sort()
-  mm=0.4
-  #print('fee',"\t",'margin',"\t",'profit05',"\t",'sale-rev',"\t",'fee-rev',"\t",'spons-cost',"\t",'members')
-  for f in fee:
-    margin=list(avgs[f].keys())
-    margin.sort()
-    maxprofit=-45456
-    maxmargin=0
-    for m in margin:
-      #mxprof
-      p=[]
-      mcost=avgs[f][m][1]*mm/(1-m)
-      profit=avgs[f][m][2]+avgs[f][m][1]-avgs[f][m][3]-mcost
-      if profit>maxprofit:
-        maxprofit=profit
-        maxmargin=m
-    print(f,round(maxprofit),maxmargin)
-      #for v in [f,m]+avgs[f][m]+[mcost,profit]:
-      #  p.append(str(round(v,2)))
-      #print("\t".join(p))
-      #print(f,"\t",m,"\t",avgs[f][m])
+  for mm1 in range(10,80,5):
+    mm=mm1/100.0
+    print('mm',mm)
+    #print('fee',"\t",'margin',"\t",'profit05',"\t",'sale-rev',"\t",'fee-rev',"\t",'spons-cost',"\t",'members')
+    for f in fee:
+      margin=list(avgs[f].keys())
+      margin.sort()
+      maxprofit=-45456
+      maxmargin=0
+      for m in margin:
+        #mxprof
+        p=[]
+        mcost=avgs[f][m][1]*mm/(1-m)
+        profit=avgs[f][m][2]+avgs[f][m][1]-avgs[f][m][3]-mcost
+        if profit>maxprofit:
+          maxprofit=profit
+          maxmargin=m
+      #print(f,round(maxprofit),maxmargin)
+      print(maxmargin)
+      print(round(maxprofit))
+        #for v in [f,m]+avgs[f][m]+[mcost,profit]:
+        #  p.append(str(round(v,2)))
+        #print("\t".join(p))
+        #print(f,"\t",m,"\t",avgs[f][m])
